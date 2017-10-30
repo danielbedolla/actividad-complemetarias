efd098f17f5948cea863aefcb0d5e804729e82dc
Daniel Macedolla Bedolla <danielbedolla97@gmail.com>
2017-10-30 15:40:37 -0600

    Se agregaron las funciones de eliminar junto a editar y se cambio el usuario root por uno local

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,8 +1,8 @@
 <?php 
 
 	$con = 'mysql:dbname=complementarias;host=localhost';
-	$user = 'root';
-	$password = '';
+	$user = 'daniel';
+	$password = 'dani123';
 
 	try {
 		$pdo = new PDO($con,$user,$password);
